b44d4b48d637b29035f239d6505683d4ade63732
Anamaria Miehs <anamaria.miehs@gmail.com>
2016-10-23 23:43:06 +0300

    a pinch of design

--- dnn/js/app.js
+++ dnn/js/app.js
@@ -4,23 +4,22 @@
 	// configure our routes
 	dnnApp.config(function($routeProvider) {
 		$routeProvider
-
 			// route for the home page
 			.when('/', {
 				templateUrl : 'pages/home.html',
 				controller  : 'mainController'
 			})
 
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
-
 			// route for the event page
 			.when('/event', {
 				templateUrl : 'pages/event.html',
 				controller  : 'eventController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
 			})
 
 	});
--- dnn/js/eventController.js
+++ dnn/js/eventController.js
@@ -1,39 +1,38 @@
 dnnApp.controller('eventController', function($scope) {
+  $scope.events = [];
 
-  $scope.event = null;
   $scope.timestamp = null;
   $scope.eventTitle = '';
   $scope.eventText = ''; 
-  $scope.eventDateTime = null;
-  $scope.date = moment();
+  $scope.date = moment().format('dddd D MMM');
   $scope.time = new Date();
 
-  $scope.events = (localStorage.getItem('events') !== null) ? JSON.parse($scope.events) : [ {timestamp: null,title: '',text: '',datetime: null} ];
-  localStorage.setItem('events', JSON.stringify($scope.events));
-  //  deleteEvents:
-  //   $scope.events = [];
-  //   localStorage.setItem('events', JSON.stringify($scope.events));
+('HH:mm');
+  // $scope.events = (localStorage.getItem('events') !== null) ? JSON.parse($scope.events) : [ {timestamp: null,title: '',text: '',datetime: null} ];
+  // localStorage.setItem('events', JSON.stringify($scope.events));
     
   $scope.resetEvent = function() {
     $scope.timestamp = null;
-    $scope.event = null;
+
     $scope.eventTitle = '';
     $scope.eventText = ''; 
     $scope.eventDateTime = null;
-    $scope.date = moment();
+    $scope.date = moment().format('dddd D MMM');
     $scope.time = new Date();
   }
 
   $scope.createEvent = function() {
-    $scope.eventDateTime = $scope.setEventDateTime($scope.date, $scope.time);
+    // $scope.eventDateTime = $scope.setEventDateTime($scope.date, $scope.time);
     $scope.events.push({
 			timestamp: $scope.setTimestamp(),
       title: $scope.eventTitle,
       text: $scope.eventText,
-      datetime: $scope.eventDateTime
+      date: $scope.date,
+      time: $scope.time
 		});
     
     $scope.resetEvent();
+
     localStorage.setItem('events', JSON.stringify($scope.events));
   }
 
@@ -52,23 +51,41 @@ dnnApp.controller('eventController', function($scope) {
     return moment().unix();
   };
 
-  $scope.setEventDateTime = function(date, time){
+  $scope.setEventDate = function(date){
     var dt;
     (typeof date === 'string') ? dt = moment(date,"ddd MMM Do yyyy HH:mm:ss ZZ") : date;
+    dt.format('dddd D MMM');
+    $scope.date = dt;
+    return $scope.date;
+  }
+
+$scope.setEventTime = function(time){
     if (typeof time !== 'Moment') {
       var t = time.getTime();
        time = undefined;
-       time = moment(t);
+       time = moment(time);
+       time.format('HH:mm');
     }
-    //"Wed Oct 19 2016 00:00:00 GMT+0300 (GTB Daylight Time)"
-    var h = time.hours();
-    var m = time.minutes();
-    dt.hours(h);
-    dt.minutes(m);
-    dt.seconds(0);
-    $scope.eventDateTime = dt;
-
-    return $scope.eventDateTime;
+    $scope.time = time;
+    return $scope.time;
   }
+  // $scope.setEventDateTime = function(date, time){
+  //   var dt;
+  //   (typeof date === 'string') ? dt = moment(date,"ddd MMM Do yyyy HH:mm:ss ZZ") : date;
+  //   if (typeof time !== 'Moment') {
+  //     var t = time.getTime();
+  //      time = undefined;
+  //      time = moment(t);
+  //   }
+  //   //"Wed Oct 19 2016 00:00:00 GMT+0300 (GTB Daylight Time)"
+  //   var h = time.hours();
+  //   var m = time.minutes();
+  //   dt.hours(h);
+  //   dt.minutes(m);
+  //   dt.seconds(0);
+  //   $scope.eventDateTime = dt;
+
+  //   return $scope.eventDateTime;
+  // }
 
 });
