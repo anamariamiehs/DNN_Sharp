785ce962db3ff5115bb29be4c981f5b70b153431
Anamaria Miehs <anamaria.miehs@gmail.com>
2016-10-28 18:29:37 +0300

    TODO: intercept the keypresses somehow and apply make-up to that ugly cube you just pushed.

--- dnn/js/mainController.js
+++ dnn/js/mainController.js
@@ -1,5 +1,15 @@
 // create the controller and inject Angular's $scope
 dnnApp.controller('mainController', function($scope) {
-    // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
+    $scope.key = function($event){
+        console.log($event.keyCode);
+        if ($event.keyCode == 38)
+            console.log("up arrow");
+        else if ($event.keyCode == 39)
+            console.log("right arrow");
+        else if ($event.keyCode == 40)
+            console.log("down arrow");
+        else if ($event.keyCode == 37)
+            console.log("left arrow");
+    }
 });
